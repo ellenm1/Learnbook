226486af7712b2e7a6f1c107ec5c07af631146d2
emeiselm <emeiselm@med.umich.edu>
2017-07-24 14:38:56 -0400

    test

--- js-local/pageArray.js
+++ js-local/pageArray.js
@@ -1,40 +1,25 @@
 /***IMPORTANT***
-	The page array generator (https://mlearningcontent2.med.umich.edu/quiz/cbtlib/modules/common/js/pa/) should be used to 
+	The page array generator (http://mlearning.med.umich.edu/quiz/cbtlib/modules/common/js/pa/) should be used to 
 	create your page array. */
 	
 var PageArray = new Array(  
-{buttonTitle:'0 Chap. 0 Competency Criteria', title:' ', url:'page01.htm', chapter:0,level:1 },
-{buttonTitle:'1 Chap. 1: Learning Objectives', title:' ', url:'page02.htm', chapter:1,level:1 },
-{buttonTitle:'Qualtrics Quiz', title:'', url:'qualtricsWrap.htm?href=https://umichumhs.qualtrics.com/SE/?SID=SV_3f9NbyBQOfTcqxf', chapter:1, level:1, type:'U', quiz:'392025628426', qmax:2, rm:'', countscore:'1'},
-{buttonTitle:'1 Title goes here', title:' ', url:'page02.2.htm', chapter:2,level:1 },
-{buttonTitle:'2 Chap. 3 Title goes here', title:' ', url:'page04.htm', chapter:3,level:1 },
-{buttonTitle:'3 Quiz', title:'', url:'quizWrap.htm?call=embed&session=1140870896389821&href=http://uhqmarkappspr1.med.umich.edu/perception5/session.php', chapter:4, level:1, type:'Q', quiz:'1140870896389821', rm:'', countscore:'1'},
-{buttonTitle:'4 Title goes here', title:' ', url:'page08.htm', chapter:5,level:1 },
-{buttonTitle:'5 Title goes here', title:' ', url:'TOC8.htm', chapter:5,level:1 },
-{buttonTitle:'6 Title goes here', title:' ', url:'page02.1.htm', chapter:1,level:1 },
-{buttonTitle:'7 Score &amp; Status Page', title:'',url:'scorePage.htm',chapter:6,level:1 }
+
+{buttonTitle:'Intro Page', title:' ', url:'page01.htm', chapter:0,level:1 },
+{buttonTitle:'Kaltura xAPI Video Player', title:' ', url:'page02.htm', chapter:1,level:1 }
+ 
 //IMPORTANT!!! the last item does NOT get a comma at the end.
 ); 	
 	
 var moduletype=2;
 
-var docTitle=('MLearning SCORM-compatible HTML template');
-var headerTitle=('MLearning SCORM-compatible HTML template');
-var contentExpert=new Array( {email:"isturis@med.umich.edu",name:"Ilza Hallman"}, {email:"winniewo@med.umich.edu",name:"Winnie Wood"}, {email:"marybov@med.umich.edu",name:"Mary Berry Bovia"} );
+var docTitle=('Kaltura Video xAPI experiments');
+var headerTitle=('Kaltura Video xAPI experiments');
+var contentExpert=new Array( {email:"emeiselm@med.umich.edu",name:"Ellen Meiselman"} );
 //var contentExpertEmail =("example@med.umich.edu");
 //var contentExpertName  =("Jane Smith");
 
 var recommendedMsg = 'Recommended';
 var requiredMsg = 'Must be completed to finish module';
 var completedMsg   = 'Completed';
-
-//var IntArray = new Object();
-//IntArray['p01_000'] =  {id:'p01_000', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'469585915910'},
-//IntArray['p01_001'] =  {id:'p01_001', tries:0, ascore:0, amax:1, req:2, msg:'', status:0, quiz:'469585915910'},
-////IntArray['p02_001'] =  {id:'p02_001', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'409666286655'},
-//IntArray['p03_000'] =  {id:'p03_000', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'409666286641'},
-//IntArray['p03_001'] =  {id:'p03_001', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'409666286641'}
-//IntArray['p05_000'] =  {id:'p05_000', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'5679585915911'},
-//IntArray['p05_001'] =  {id:'p05_001', tries:0, ascore:0, amax:1, req:2, msg:'', status:0, quiz:'5679585915911'}
 
  
