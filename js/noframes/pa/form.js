46cf5ff832cc5f5c7268e5a0f4590ddfd8348033
Ellen Meiselman <ellenm1@gmail.com>
2021-02-25 12:41:55 -0500

    various partially completed changes to pagearray generator

--- js/noframes/pa/form.js
+++ js/noframes/pa/form.js
@@ -484,8 +484,8 @@ function toggle(selectedLayer, searchclass){
     				}
         });
        	
-       	
-       	var whatIsAdvancedSettings = '<p>Advanced settings are items that rarely need to be used, such as changing or shutting off the blue timeout warnings, turning on and customizing auto-emails, etc. <p>';
+ //  Use this instead of hovercard: https://tutorialzine.com/2014/07/css-inline-help-tips      	
+  /*     	var whatIsAdvancedSettings = '<p>Advanced settings are items that rarely need to be used, such as changing or shutting off the blue timeout warnings, turning on and customizing auto-emails, etc. <p>';
         var hidebookmarkalertslabel = '<p>\"Do you want to resume?\" alerts are shown on launch if a module has more than 5 pages. If there are Captivate or Storyline quizzes, this checkbox is automatically checked. This is because you may want to mute these alerts since Captivate and Storyline have their own Resume functions.</p>';
     $("#advanced-settings").hovercard({
         detailsHTML: whatIsAdvancedSettings,
@@ -498,6 +498,7 @@ function toggle(selectedLayer, searchclass){
         width: 400,
         openOnTop:true
     });  
+    */
         
  
     });  //end document ready function
