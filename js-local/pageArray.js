5da003fcab6b68ec83b07e35c50f5464d63382a3
Ellen Meiselman <ellenm1@gmail.com>
2023-11-17 14:01:40 -0500

    added warning message turn-off for cornerstone

--- js-local/pageArray.js
+++ js-local/pageArray.js
@@ -3,16 +3,17 @@
 	create your page array. */
 	
 var PageArray = new Array(  
-{buttonTitle:'0 Chap. 0 Competency Criteria', title:' ', url:'page01.htm', chapter:0,level:1 },
-{buttonTitle:'1 Chap. 1: Learning Objectives', title:' ', url:'page02.htm', chapter:1,level:1 },
-{buttonTitle:'Qualtrics Quiz', title:'', url:'qualtricsWrap.htm?href=https://umichumhs.qualtrics.com/SE/?SID=SV_3f9NbyBQOfTcqxf', chapter:1, level:1, type:'U', quiz:'392025628426', qmax:2, rm:'', countscore:'1'},
-{buttonTitle:'1 Title goes here', title:' ', url:'page02.2.htm', chapter:2,level:1 },
-{buttonTitle:'2 Chap. 3 Title goes here', title:' ', url:'page04.htm', chapter:3,level:1 },
-{buttonTitle:'3 Quiz', title:'', url:'quizWrap.htm?call=embed&session=1140870896389821&href=http://uhqmarkappspr1.med.umich.edu/perception5/session.php', chapter:4, level:1, type:'Q', quiz:'1140870896389821', rm:'', countscore:'1'},
-{buttonTitle:'4 Tab example', title:' ', url:'tabExample.htm', chapter:5,level:1 },
-{buttonTitle:'5 Title goes here', title:' ', url:'TOC8.htm', chapter:5,level:1 },
-{buttonTitle:'6 Title goes here', title:' ', url:'page02.1.htm', chapter:1,level:1 },
-{buttonTitle:'7 Score &amp; Status Page', title:'',url:'scorePage.htm',chapter:6,level:1 }
+{buttonTitle:'0 Competency Criteria', title:' ', url:'page01.htm', chapter:1,level:1 },
+{buttonTitle:'1 Learning Objectives', title:' ', url:'page02.htm', chapter:1,level:1 },
+//{buttonTitle:'2 Qualtrics Quiz', title:'', url:'qualtricsWrap.htm?href=https://umichumhs.qualtrics.com/SE/?SID=SV_3f9NbyBQOfTcqxf', chapter:1, level:1, type:'U', quiz:'392025628426', qmax:2, rm:'', countscore:'1'},
+{buttonTitle:'2 Title goes here', title:' ', url:'page02.2.htm', chapter:1,level:1 },
+{buttonTitle:'3 Title goes here', title:' ', url:'page04.htm', chapter:1,level:1 },
+{buttonTitle:'4 Title goes here', title:' ', url:'tabExample.htm', chapter:1,level:1 },
+{buttonTitle:'5 Title goes here', title:' ', url:'TOC8.htm', chapter:1,level:1 },
+//{buttonTitle:'6 Title goes here', title:' ', url:'page02.1.htm', chapter:1,level:1 },
+{buttonTitle:'6 Quiz', title:'', url:'quizWrap.htm?call=embed&session=1140870896389821&href=http://uhqmarkappspr1.med.umich.edu/perception5/session.php', chapter:1, level:1, type:'Q', quiz:'1140870896389821', rm:'', countscore:'1'},
+
+{buttonTitle:'7 Score &amp; Status Page', title:'',url:'scorePage.htm',chapter:1,level:1 }
 //IMPORTANT!!! the last item does NOT get a comma at the end.
 ); 	
 	
@@ -28,6 +29,11 @@ var contentExpertEmail =('test@test.com');
 var recommendedMsg = 'Recommended';
 var requiredMsg = 'Must be completed to finish module';
 var completedMsg   = 'Completed';
+
+var muteStartupMsg=true;
+var hidescorepagewarning=true;
+
+
 
 //var IntArray = new Object();
 //IntArray['p01_000'] =  {id:'p01_000', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'469585915910'},
